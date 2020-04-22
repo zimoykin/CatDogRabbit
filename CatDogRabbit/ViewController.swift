13d658ebab3f4d2f1d78be29228312ba0cdb111c
Dmitry Zimoykin <zimoykin@icloud.com>
2020-04-22 22:29:02 +0300

    Initial Commit

---
+++ CatDogRabbit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatDogRabbit
+//
+//  Created by Дмитрий on 22.04.2020.
+//  Copyright © 2020 DZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
